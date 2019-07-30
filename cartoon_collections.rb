7805f65d0034e774328be7977233bb6b5a6ebf33
Renee Sims <info@reneesoul.com>
2019-07-30 18:41:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,10 @@ def summon_captain_planet(names)
   names.map! do |name| p name.capitalize + '!' end
 end
 
-
+def find_the_cheese(string)
+  cheese = string.to_a
+  
+end
 
 def long_planeteer_calls(array)
   array.find do |name| 
